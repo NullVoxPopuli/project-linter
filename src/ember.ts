a181f17f052c98ec945198d5ac8056e2bd960d05
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-06-14 23:01:11 -0400

    eh

--- src/ember.ts
+++ src/ember.ts
@@ -4,18 +4,54 @@ import path from 'path';
 import ts from 'jscodeshift/parser/ts';
 import js from 'jscodeshift/parser/babylon';
 
+export async function readFile(filePath: string) {
+  let source = await fs.readFile(filePath, { encoding: 'utf-8' });
+  let parser = parserFor(filePath);
+
+  return { source, parser };
+}
+
+export function parserFor(filePath: string) {
+  let ext = path.extname(filePath);
+
+  switch (true) {
+    case ext === '.js':
+      return js();
+    case ext === '.ts':
+      return ts();
+  }
+}
 
 export function isTemplate(filePath: string) {
   let ext = path.extname(filePath);
 
-  return ext.endsWith('hbs');
+  return ext === '.hbs';
 }
 
-export function isComponentJs(filePath: string) {
+export async function isComponentJs(filePath: string) {
+  let { source, parser } = await readFile(filePath);
+
+  if (!parser) {
+    return false;
+  }
+
+
+  let root = parser.parse(source);
+
+  root.find('ImportDeclaration').forEach(i => {
+    console.log(i);
+  });
 
 }
 
 export function isRoute(filePath: string) {
+  let { source, parser } = await readFile(filePath);
+
+  if (!parser) {
+    return false;
+  }
+
+
 
 }
 
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,29 @@
 interface Dict<T> {
   [key: string]: T;
 }
+
+
+
+declare module 'jscodeshift/parser/babylon' {
+  type File = import('@babel/types').File
+
+  interface Parser {
+    parse: (code: string) => File,
+  }
+
+  const ParseMaker: (/* always default */) => Parser;
+
+  export default ParseMaker;
+}
+
+declare module 'jscodeshift/parser/ts' {
+  type File = import('@babel/types').File
+
+  interface Parser {
+    parse: (code: string) => File,
+  }
+
+  const ParseMaker: (/* always default */) => Parser;
+
+  export default ParseMaker;
+}
